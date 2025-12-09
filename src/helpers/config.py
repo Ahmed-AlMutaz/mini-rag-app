08b501c46b542dbc2c170dadf7879c30fde027a5
Ahmed Almoataz Bellah Alkamil <2200364@student.eelu.edu.eg>
2025-12-09 16:11:07 +0200

    Add initial database schema models for project and data_chunk
    - Created __init__.py to facilitate module imports for db_schemes.
    - Implemented data_chunk model with fields for chunk text, metadata, order, and project ID.
    - Developed project model with validation for alphanumeric project IDs.
    - Configured models to allow arbitrary types for compatibility with ObjectId.

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -5,9 +5,13 @@ class Sittings(BaseSettings):
     app_name : str 
     app_version : str 
     OPENAIKEY : str
+    
     File_Allowed_Types: list
     File_Max_Size_MB: int
     File_Default_Chunk_Size: int
+
+    MongoDB_URI: str
+    MongoDB_Database: str
 
 
     class Config:
---
+++ src/models/db_schemes/project.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel , Field , validator
+from typing import Optional
+from bson.objected import ObjectId
+
+
+class project(BaseModel):
+    _id : Optional[ ObjectId ]
+    project_id : str = Field(..., min_length = 1)
+
+    @validator('project_id')
+    def validate_project_id(cls , value):
+        if not value.isalnum():
+            rise ValueError('projet_id must be Alphanumeric')
+
+        return value
+    
+
+    class Config : 
+        arbitary_types_allowed = True
